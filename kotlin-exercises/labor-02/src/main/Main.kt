821cc1213b92f7a12fc381f4ae7ed835788d76a9
Akos Szabo <akossz12@gmail.com>
2024-10-02 18:57:23 +0300

    first problem solved

--- kotlin-exercises/labor-02/src/main/Main.kt
+++ kotlin-exercises/labor-02/src/main/Main.kt
@@ -1,9 +1,41 @@
-package main
+fun main() {
+    // Problem 1
+    val dict: IDictionary = ListDictionary()
+    createMockDictData(dict)
+    println("Number of words: ${dict.size()}")
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+    val searchWords = listOf("apple", "pear", "banana", "watermelon", "kiwi", "mango")
+    for (word in searchWords) {
+        println("Searching for '$word': Result: ${dict.find(word)}")
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+// Problem 1
+interface IDictionary {
+    fun add(word: String)
+    fun find(word: String): Boolean
+    fun size(): Int
+}
+
+class ListDictionary : IDictionary {
+    private val words: MutableList<String> = mutableListOf()
+
+    override fun add(word: String) {
+        words.add(word)
+    }
+
+    override fun find(word: String): Boolean {
+        return words.contains(word)
+    }
+
+    override fun size(): Int {
+        return words.size
+    }
+}
+
+fun createMockDictData(dict: IDictionary) {
+    val mockWords = listOf("apple", "banana", "grape", "orange", "melon", "kiwi", "mango", "cherry", "pear", "peach")
+    for (word in mockWords) {
+        dict.add(word)
+    }
 }
