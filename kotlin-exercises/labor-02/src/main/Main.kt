e3980c2a018c0835e867bee3e9fe118fe26a1dba
Akos Szabo <akossz12@gmail.com>
2024-10-02 19:03:52 +0300

    problem 2 solved

--- kotlin-exercises/labor-02/src/main/Main.kt
+++ kotlin-exercises/labor-02/src/main/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     // Problem 1
+    println("Problem 1")
     val dict: IDictionary = ListDictionary()
     createMockDictData(dict)
     println("Number of words: ${dict.size()}")
@@ -8,9 +9,20 @@ fun main() {
     for (word in searchWords) {
         println("Searching for '$word': Result: ${dict.find(word)}")
     }
+    println("\n")
+    
+    // Problem 2
+    val fullName = "John Smith"
+    println("Monogram of '$fullName': ${fullName.monogram()}")
+	
+    val fruits = listOf("apple", "pear", "melon")
+    val separator = "-"
+    println("Joined fruits: ${fruits.joinWithSeparator(separator)}")
+    
+    val words = listOf("apple", "pear", "strawberry", "melon")
+    println("Longest word: ${words.longestString()}")
 }
 
-// Problem 1
 interface IDictionary {
     fun add(word: String)
     fun find(word: String): Boolean
@@ -39,3 +51,22 @@ fun createMockDictData(dict: IDictionary) {
         dict.add(word)
     }
 }
+
+// Problem 2
+fun String.monogram(): String {
+    return this.split(" ")
+    	.map { it.first() }
+        .joinToString("")
+}
+
+fun List<String>.joinWithSeparator(separator: String): String {
+    return this.joinToString(separator)
+}
+
+fun List<String>.longestString(): String? {
+    return this.maxByOrNull { it.length }
+}
+
+
+
+
