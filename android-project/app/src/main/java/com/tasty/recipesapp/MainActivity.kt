4d90047d7dd667935f407670e2a137a644ac29de
Akos Szabo <akossz12@gmail.com>
2024-10-30 19:52:44 +0200

    created nav

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.tasty.recipesapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Toast
-import android.widget.Button
-import android.os.Build
 import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +24,28 @@ class MainActivity : AppCompatActivity() {
             window.statusBarColor = Color.parseColor("#f15025")
         }
 
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
-        getStartedButton.setOnClickListener {
-            Toast.makeText(this, "Let's get started!", Toast.LENGTH_SHORT).show()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
 
-            // val intent = Intent(this, OnboardingActivity::class.java)
-            // startActivity(intent)
+        // Set OnItemSelectedListener for the BottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            // Handle the selection of navigation items
+            when (item.itemId) {
+                R.id.homeFragment -> {  // Ensure these IDs match the menu resource
+                    navController.navigate(R.id.homeFragment)
+                    true // Return true to indicate that the selection was handled
+                }
+                R.id.recipesFragment -> {
+                    navController.navigate(R.id.recipesFragment)
+                    true
+                }
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+                else -> false // Return false if the selection wasn't handled
+            }
         }
     }
 }
